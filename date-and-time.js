b48432a8496bbc1daa05a82b257cfdb2b389bcda
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-24 17:23:06 -0400

    created a variable that can print seconds of the day

--- date-and-time.js
+++ date-and-time.js
@@ -2,6 +2,8 @@ let moment = require("moment");
 
 let fullTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 let days = moment().format("DDDo");
+let seconds = moment().seconds() + (moment().minute() * 60) + (moment().hours() * 60 * 60);
 
 console.log(`It is ${fullTime}.`);
 console.log(`It is ${days} day of the year.`);
+console.log(`It is ${seconds} seconds into the day.`);
