8d4b029ebbc952b12dca02130fd4abdced3777ee
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-24 17:53:51 -0400

    added color to variables used in console.log

--- date-and-time.js
+++ date-and-time.js
@@ -1,9 +1,22 @@
-let moment = require("moment");
+const chalk = require("chalk");
 
+let moment = require("moment");
 let fullTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 let days = moment().format("DDDo");
 let seconds = moment().seconds() + (moment().minute() * 60) + (moment().hours() * 60 * 60);
+let dst = moment().isDST();
+let daylight = "is not";
+if (dst) {
+  daylight = "is";
+}
+let leapYear = moment().isLeapYear();
+let year = "is not";
+if (leapYear) {
+  year = "is";
+}
 
-console.log(`It is ${fullTime}.`);
-console.log(`It is ${days} day of the year.`);
-console.log(`It is ${seconds} seconds into the day.`);
+console.log(`It is ${chalk.blue(fullTime)}.`);
+console.log(`It is ${chalk.magenta(days)} day of the year.`);
+console.log(`It is ${chalk.cyan(seconds)} seconds into the day.`);
+console.log(`It ${chalk.green(daylight)} during Daylight Savings Time.`);
+console.log(`It ${chalk.red(year)} a leap year.`);
