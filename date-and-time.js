c93a691f5bb828230bd0c9633f6c4111da9f987c
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-24 16:22:48 -0400

    created variable that can be used to print out the day of the year

--- date-and-time.js
+++ date-and-time.js
@@ -1,5 +1,7 @@
 let moment = require("moment");
 
 let fullTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+let days = moment().format("DDDo");
 
-console.log(`It is ${fullTime}`);
+console.log(`It is ${fullTime}.`);
+console.log(`It is ${days} day of the year.`);
